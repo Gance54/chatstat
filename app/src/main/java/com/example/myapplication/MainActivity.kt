5cef0102db3c24f01143d8a7280446385df4bc3b
Mykhailo Sopiha <gance54@gmail.com>
2020-02-18 00:12:02 +0200

    Add permission and log file
    Signed-off-by: Mykhailo Sopiha <gance54@gmail.com>

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,48 @@
 package com.example.myapplication
 
+import android.Manifest
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 import org.drinkless.td.libcore.telegram.Client
+import org.drinkless.td.libcore.telegram.TdApi
+
+import java.io.IOError;
+import java.io.IOException;
+import androidx.core.app.ComponentActivity
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+import android.app.AlertDialog
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.os.Environment
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
+
+    val gPermissionGranted = 10
+
+    fun alertDlg(message: String) {
+        val builder = AlertDialog.Builder(this)
+
+        builder.setMessage(message)
+            .setTitle("Info")
+
+        val dialog = builder.create()
+        dialog.show()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,5 +69,88 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    fun runLogs() {
+        val err = Client.execute(
+            TdApi.SetLogStream(
+                TdApi.LogStreamFile(
+                    getExternalFilesDir("logs").toString() + "log",
+                    (1 shl 27).toLong()
+                )
+            ))
+
+        if (err is TdApi.Error) {
+            throw IOError(IOException("Error has occurred. " + err.message))
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            gPermissionGranted -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    alertDlg("Granted. Logs will be here: " + getExternalFilesDir("logs").toString())
+                } else {
+                    alertDlg("GIVE ME PERMISSIONS BITCH!!")
+                }
+                return
+            }
+
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.INTERNET,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE),
+            gPermissionGranted)
+    }
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                val builder = AlertDialog.Builder(this)
+                builder.setMessage("Permission to access files is required for this app to write logs.")
+                        .setTitle("Permission required")
+                            builder.setPositiveButton("OK"
+                            ) { dialog, id ->
+                        makeRequest()
+                    }
+
+                    val dialog = builder.create()
+                dialog.show()
+            } else {
+                makeRequest()
+            }
+        }
+        else {
+            runLogs()
+        }
+    }
+
+    fun logIn (view: View) {
+        Client.execute(TdApi.SetLogVerbosityLevel( 3))
+        setupPermissions()
+
+        //InfoText.setText("Login started. Please verify your account.");
+        //InputCode.setText(null);
+        //InputCode.setHint("Enter validation code")
+    }
+
+    private fun confirm (view: View) {
+        InfoText.setText(InputCode.text)
     }
 }
